b3f8893a662fa95218d763da40129817914c5010
Ayan Bishnoi <102575458+Ayan209e@users.noreply.github.com>
2023-12-04 05:29:57 +0000

    comparisons

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -24,3 +24,31 @@ let someNumber = 100;
 let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+// ******************************** Operations **********************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// Shift + Alt + DownKey
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "hello ";
+let str2 = "ayan";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log("1" + 2 + "2"); // 32 
+
+// console.log(+true); // 1
+// console.log(+""); // 1
